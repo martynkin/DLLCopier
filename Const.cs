4189e09ca651ba5d94ff060e29117a60c0a57df1
martynkin <Alexey.Martynkin@smartbear.com>
2015-06-03 14:28:54 +0300

    commit 2

--- Const.cs
+++ Const.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DllCopier
 {
-asdsadasdasd
+///asdsadasdasd
+///asdsadasdasd
     static class Const
     {
         public static readonly string[] XE8x32 = { "XE8", "32", "rtl220.bpl", "CC32160MT.dll", "Borlndmm.dll" };
