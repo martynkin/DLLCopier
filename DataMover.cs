722be096b86db957c79b4b6b2f5b87f0766443f8
martynkin1 <martynkin@mail.ru>
2015-06-08 16:40:05 +0300

    Update DataMover.cs

--- DataMover.cs
+++ DataMover.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace DllCopier
 {
-
+//eeewerwrwerwer
+//werwerwer
+///werwerwerwerwerwer
         public List<string> DestPathes;
         public Dictionary<int, List<string>> DllNames;
         public string DllFolder;
