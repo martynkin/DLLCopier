aa91375f4eef47734143d233f51f4c7a5a02dbed
martynkin <Alexey.Martynkin@smartbear.com>
2015-06-11 17:29:13 +0300

    Update Const.cs

--- Const.cs
+++ Const.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace DllCopier
 {
-//dasdasdasdasdasd
-//asdasdasdasdasdasdasd
-//asdasdasdasdasdasdasd
-//asdasdasdasdasdsad
+
 
     static class Const
     {
